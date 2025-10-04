9ca38a1a7250bd9a69abdb6c4b65ed4dd6991b78
Rol123-m <rol123-m.github.io.com>
2025-10-04 18:50:58 -0400

    yes

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',  // Cambié esto a './'
+  base: '/koine-greek-apk/', // ¡IMPORTANTE! Añadir esta línea
   server: {
     port: 3000,
-  },
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    emptyOutDir: true
   }
 })
